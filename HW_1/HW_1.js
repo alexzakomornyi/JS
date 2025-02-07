db8fbf97599ae6c2d990b9f13b03433564eee438
alexzakomornyi <zakalex522@gmail.com>
2025-02-07 17:35:34 +0200

    create JS directory, create HW_1 directory, add HW_1

---
+++ HW_1/HW_1.js
@@ -0,0 +1,62 @@
+// #dYQNrBV
+let hello = 'hello';
+console.log(hello);
+
+let owu = 'owu';
+console.log(owu);
+
+let com = 'com';
+console.log(com);
+
+let d = 'ua';
+console.log(d);
+
+let e = 1;
+console.log(e);
+
+let f = 10;
+console.log(f);
+
+let g = -99;
+console.log(g);
+
+let h = 123;
+console.log(h);
+
+let i = 3.14;
+console.log(i);
+
+let j = 2.7;
+console.log(j);
+
+let k = 16;
+console.log(k);
+
+let l = true;
+console.log(l);
+
+let m = false;
+console.log(m);
+
+// #6Qb97gsv
+let firstName = 'Oleksii';
+let middleName = 'Vitaliyovich';
+let lastName = 'Zakomornyi';
+let fullName = `${lastName} ${firstName} ${middleName}`;
+console.log(fullName);
+
+// #4N0y5tufA
+let a = 100;
+console.log(typeof(a));
+
+let b = '100';
+console.log(typeof(b));
+
+let c = true;
+console.log(typeof(c));
+
+//#ruUtWDUI
+let userName = prompt('What is your name?');
+let userMiddleName = prompt('What is your middle name?');
+let userLastName = prompt('What is your last name?');
+console.log(`I'm ${userLastName} ${userName} ${userMiddleName}`);
